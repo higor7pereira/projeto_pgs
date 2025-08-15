d350ea0d837c64ee77ff8fa8d5718e3cfdbdb3d2
higor7pereira <higor.l.pereira@gmail.com>
2025-08-15 01:36:26 +0000

    versão 2

--- OlaMundo.java
+++ OlaMundo.java
@@ -2,5 +2,6 @@ public class OlaMundo {
    public static void main(String[] args){
     System.out.println("Hello world");
     System.err.println("Programação de Sistemas I");
+    System.System.out.println("Boa Noite");
    }
 }
